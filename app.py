d656e3b3d1afd83f967f72dd46f114f48112d479
Fernando-5D <23308060610105@cetis61.edu.mx>
2025-11-02 20:56:58 -0700

    commit

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 # aviso nutrimental (azucares o sales altas en alimentos, productos o recetas)
+import requests
 from flask import Flask, render_template
 app = Flask(__name__)
 
+apiKey = "4988525657d749cd88a7104290c42ed0"
 @app.route("/")
 def inicio():
-    return render_template("inicio.html")
+    url_randomRecipes = "https://api.spoonacular.com/recipes/random?includeNutrition=false"
+    recetas = requests.get(url_randomRecipes, params = {"apiKey": apiKey, "number": 5}).json()
+    return render_template("inicio.html", recetas=recetas)
 
 if __name__ == "__main__":
     app.run(debug=True)
