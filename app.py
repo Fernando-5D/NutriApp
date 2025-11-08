07609c4522645bae2402f9e0eb60cf3dedc6b2ac
Fernando-5D <23308060610105@cetis61.edu.mx>
2025-11-08 15:14:55 -0700

    commit

--- app.py
+++ app.py
@@ -19,7 +19,21 @@ def iniciandoSesion():
         if correo in usuarios:
             passw = request.form.get("passw")
             if passw == usuarios[correo].passw:
+                nombre = usuarios[correo].nombre
+                fechaNacim = usuarios[correo].fechaNacim
+                genero = usuarios[correo].genero
+                peso = usuarios[correo].peso
+                altura = usuarios[correo].altura
+                actFisica = usuarios[correo].actFisica
+                
+                session["nombre"] = nombre
+                session["fechaNacim"] = fechaNacim
+                session["genero"] = genero
+                session["peso"] = peso
+                session["altura"] = altura
+                session["actFisica"] = actFisica
                 session["correo"] = correo
+                session["passw"] = passw
             else:
                 flash("La contraseña es incorrecta.")
         else:
