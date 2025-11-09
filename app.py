08229f0273ce9e46efe809390490cda2b1631c4e
Fernando-5D <23308060610105@cetis61.edu.mx>
2025-11-09 00:03:01 -0700

    commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # aviso nutrimental (azucares o sales altas en alimentos, productos o recetas)
-import datetime
+from datetime import datetime, date
 from flask import Flask, render_template, request, flash, get_flashed_messages, redirect, url_for, session
 app = Flask(__name__)
 
@@ -20,20 +20,20 @@ def iniciandoSesion():
         correo = request.form.get("correo")
         if correo in usuarios:
             passw = request.form.get("passw")
-            if passw == usuarios[correo].passw:
-                session["nombre"] = usuarios[correo].nombre
-                session["genero"] = usuarios[correo].genero
-                session["fechaNacim"] = usuarios[correo].fechaNacim
-                session["edad"] = usuarios[correo].edad
-                session["actFisica"] = usuarios[correo].actFisica
-                session["peso"] = usuarios[correo].peso
-                session["altura"] = usuarios[correo].altura
-                session["correo"] = usuarios[correo].correo
-                session["passw"] = usuarios[correo].passw
+            if passw == usuarios[correo]["passw"]:
+                session["nombre"] = usuarios[correo]["nombre"]
+                session["genero"] = usuarios[correo]["genero"]
+                session["fechaNacim"] = usuarios[correo]["fechaNacim"]
+                session["edad"] = usuarios[correo]["edad"]
+                session["actFisica"] = usuarios[correo]["actFisica"]
+                session["peso"] = usuarios[correo]["peso"]
+                session["altura"] = usuarios[correo]["altura"]
+                session["correo"] = usuarios[correo]["correo"]
+                session["passw"] = usuarios[correo]["passw"]
             else:
-                flash("La contraseña es incorrecta")
+                flash("La contraseña es incorrecta", "danger")
         else:
-            flash("No se encontro el usuario, ingresaste el correo correctamente?")
+            flash("No se encontro el usuario, ingresaste el correo correctamente?", "danger")
         
         if get_flashed_messages():
             return render_template("sesion.html")
@@ -50,7 +50,7 @@ def registrando():
     if request.method == "POST":
         nombre = request.form.get("nombre")
         genero = request.form.get("genero")
-        fechaNacim = datetime.strptime(request.form["fechaNac"], '%Y-%m-%d').date()
+        fechaNacim = datetime.strptime(request.form["fechaNacim"], '%Y-%m-%d').date()
         actFisica = request.form.get("actFisica")
         peso = request.form.get("peso")
         altura = request.form.get("altura")
@@ -70,7 +70,7 @@ def registrando():
         if not actFisica:
             error.append("Selecciona tu nivel de actividad fisica")
         
-        if usuarios[correo]:
+        if usuarios.get(correo):
             error.append("El correo ingresado ya esta siendo usado por otra cuenta")
         
         if passwC != passw:
@@ -78,8 +78,8 @@ def registrando():
 
         if error:
             for err in error:
-                flash(err)
-            return render_template("registrate.html")
+                flash(err, "danger")
+            return render_template("registro.html")
         else:
             usuarios[correo] = {
                 "nombre": nombre,
@@ -93,7 +93,7 @@ def registrando():
                 "passw": passw
             }
 
-            flash(f"Registro exitoso para el usuario: {email}", "success")
+            flash(f"Registrado con exito: {correo}", "success")
             return redirect(url_for("sesion"))
 
 if __name__ == "__main__":
